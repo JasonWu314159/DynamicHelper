287c8e5e16486295adf3878907ac971e8be19429
JasonWu314159 <cpa950612@gmail.com>
2025-06-05 13:23:14 +0800

    修復充電動畫bug

--- DynamicHelper/componet/BatteryView.swift
+++ DynamicHelper/componet/BatteryView.swift
@@ -243,6 +243,10 @@ final class PowerMonitor {
 
 struct InputAnimation: View  {
     @State var spacing: CGFloat = 0
+    @State private var isPluggedIn: Bool = false
+    @State private var timer: Timer?
+    @State private var isShowInputAnimation: Bool = false
+    @State private var AnimateQueue: [Bool] = []
     var body: some View {
         GeometryReader { geometry in
             HStack(spacing: 10) {
@@ -259,13 +263,59 @@ struct InputAnimation: View  {
         }
         .clipped()
         .onAppear {
-            guard let batteryInfo = PowerMonitor.getBatteryInfo() else { return }
-            self.spacing = batteryInfo.isPluggedIn ? 0 : 20
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                withAnimation(.easeInOut(duration: 1.0)) {
-                    self.spacing = batteryInfo.isPluggedIn ? 20 : 0
+            startMonitoring()
+            if !isPluggedIn{
+                showInputAnimation(isPluggedIn)
+            }
+        }
+        .onChange(of: isPluggedIn) { _,batteryInfo in
+            showInputAnimation(batteryInfo)
+        }
+        .onDisappear {
+            stopMonitoring()
+        }
+    }
+    
+    func showInputAnimation(_ PluggedIn: Bool) {
+        if isShowInputAnimation {
+            if AnimateQueue.count < 1{
+                AnimateQueue.append(PluggedIn)
+            }
+            return 
+        }
+        isShowInputAnimation = true
+        let p = PluggedIn
+        self.spacing = p ? 0 : 20
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation(.easeInOut(duration: 1.0)) {
+                self.spacing = p ? 20 : 0
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                isShowInputAnimation = false
+                if AnimateQueue.count > 0 {
+                    showInputAnimation(AnimateQueue.first!)
+                    AnimateQueue.removeFirst()
                 }
             }
         }
+    }
+    
+    func startMonitoring() {
+        updateBatteryInfo() // 立即更新一次
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            updateBatteryInfo()
+        }
+    }
+
+    func stopMonitoring() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    func updateBatteryInfo() {
+        if let info = PowerMonitor.getBatteryInfo() {
+            isPluggedIn = info.isPluggedIn
+        }
+//        print(1)
     }
 }

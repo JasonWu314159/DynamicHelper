fc2117f5b7722734269af816a136cdf1b55a03d3
JasonWu314159 <cpa950612@gmail.com>
2025-06-08 18:45:50 +0800

    Initial Commit

---
+++ DynamicHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicHelper
+//
+//  Created by 吳佳昇 on 2025/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicHelper/DynamicHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicHelperApp.swift
+//  DynamicHelper
+//
+//  Created by 吳佳昇 on 2025/6/8.
+//
+
+import SwiftUI
+
+@main
+struct DynamicHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
